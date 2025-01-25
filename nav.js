bbf28d11164e5b2e9f9cfbf4a3542c12cb6c820e
Allan <matrixboston7613@gmail.com>
2025-01-26 01:03:29 +0800

    updated nav.js for toggling menu dropdown and burger

--- nav.js
+++ nav.js
@@ -1,1 +1,11 @@
-console.log("test");
+//By A11AN0 :)
+
+const burger = document.querySelector(".mainNav__container__logo__burger");
+const desktopMenu = document.querySelector(".mainNav__container__desktopMenu");
+const navContainer = document.querySelector(".mainNav__container");
+
+burger.addEventListener("click", () => {
+    burger.classList.toggle("is-active");
+    desktopMenu.classList.toggle("is-open");
+    navContainer.classList.toggle("mainNav__container--expanded");
+});
